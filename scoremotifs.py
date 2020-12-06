c6ade6f7bd6fcb99729622ee652903df57c45d5b
cvrattos1 <cvrattos@princeton.edu>
2020-12-06 12:25:23 -0500

    some GLAM results

--- scoremotifs.py
+++ scoremotifs.py
@@ -7,22 +7,26 @@ import matplotlib.pyplot as plt
 # To be finished tomorrow (basically just call all of the functions)
 def scoreAll():
 	df = pd.DataFrame()
-	folders = ['long_background', 'long_motif', 'plasmodium_falciparum', 'random_motif', 'short_motif', 'tandem_repeat']
+	folders = ['control', 'long_background', 'long_motif', 'plasmodium_falciparum', 'random_motif', 'short_motif', 'tandem_repeat']
 	for variable in folders:
 		print(variable)
 		with open(variable + '/motif.json') as f:
 			template = pd.read_json(f)
-		with open(variable + '/meme.json') as f:
-			meme = pd.read_json(f)
+		if(variable != 'tandem_repeat'):
+			with open(variable + '/meme.json') as f:
+				meme = pd.read_json(f)
+				memeScore = scoreMotif(template, meme, 0.25, 0.25, 0.25, 0.25)
+		else:
+			memeScore = float("nan")
 		with open(variable + '/GAME.json') as f:
 			game = pd.read_json(f)
-		if(variable != 'random_motif' and variable != 'tandem_repeat'):
+		if(variable != 'tandem_repeat'):
 			with open(variable + '/MITSU.json') as f:
 				mitsu = pd.read_json(f)
 				mitsuScore = scoreMotif(template, mitsu, 0.25, 0.25, 0.25, 0.25)
 		else:
 			mitsuScore = float("nan")
-		if(variable != 'random_motif'):
+		if(variable != 'tandem_repeat'):
 			with open(variable + '/qpms9.json') as f:
 				qpms9 = pd.read_json(f)
 				qpms9Score = scoreMotif(template, qpms9, 0.25, 0.25, 0.25, 0.25)
@@ -38,7 +42,6 @@ def scoreAll():
 				motifsamplerScore = scoreMotif(template, motifsampler, 0.25, 0.25, 0.25, 0.25)
 		else:
 			motifsamplerScore = float("nan")
-		memeScore = scoreMotif(template, meme, 0.25, 0.25, 0.25, 0.25)
 		gameScore = scoreMotif(template, game, 0.25, 0.25, 0.25, 0.25)
 		gemfaScore = scoreMotif(template, gemfa, 0.25, 0.25, 0.25, 0.25)
 		glam2Score = scoreMotif(template, glam2, 0.25, 0.25, 0.25, 0.25)
