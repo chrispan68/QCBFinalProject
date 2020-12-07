bf62023f541acba2bd705afe8e577855277bf1cd
cvrattos1 <cvrattos@princeton.edu>
2020-12-06 20:31:34 -0500

    reverse complement

---
+++ reversecomplement.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def generateComplement(sequence):
+	sequence.columns = ['T', 'A', 'G', 'C']
+	sequence = sequence[::-1]
+	return sequence
+
+def main():
+	with open('control/GAME.json') as f:
+		game = pd.read_json(f)
+	generateComplement(game)
+
+if __name__ == "__main__":
+	main()
